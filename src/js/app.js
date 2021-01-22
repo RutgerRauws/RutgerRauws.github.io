66fe6ec2ccaaa548d41009d5b46e5365c35d9fec
Rutger Rauws <rutgerrauws@gmail.com>
2021-01-22 21:31:21 +0100

    Fancy read more arrows

--- src/js/app.js
+++ src/js/app.js
@@ -3,18 +3,22 @@ document.addEventListener("DOMContentLoaded", function () {
   sweetScroll = new SweetScroll({/* some options */});
 }, false);
 
-$(".readmore-link").click( function(e) {
-  // record if our text is expanded
-  var isExpanded =  $(e.target).hasClass("expand");
-  
-  //close all open paragraphs
-  $(".readmore.expand").removeClass("expand");
-  $(".readmore-link.expand").removeClass("expand");
-  
-  // if target wasn't expand, then expand it
-  if (!isExpanded){
-    $( e.target ).parent( ".readmore" ).addClass( "expand" );
-    $(e.target).addClass("expand");  
-  } 
+$(".readmore-link").click(function clicked(e) {
+    // record if our text is expanded
+    var isExpanded = $(e.target).hasClass("expand");
+    
+    //close all open paragraphs
+    $(".readmore.expand").removeClass("expand");
+    $(".readmore-link.expand").children("i").addClass("fa-chevron-down");
+    $(".readmore-link.expand").children("i").removeClass("fa-chevron-up");
+    $(".readmore-link.expand").removeClass("expand");
+    
+    // if target wasn't expand, then expand it
+    if (!isExpanded) {
+        $( e.target ).parent( ".readmore" ).addClass( "expand" );
+        $(e.target).addClass("expand"); 
+        
+        $(e.target).children("i").addClass("fa-chevron-up");
+        $(e.target).children("i").removeClass("fa-chevron-down");
+    } 
 });
-
